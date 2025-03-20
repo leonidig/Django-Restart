826a6361c00f903194b821b4eca2bf094f194f56
Leonidig <leonid200955@gmail.com>
2025-03-20 21:00:40 +0200

    done

--- catalog/products/views.py
+++ catalog/products/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+from .models import Product
+
 
 def index(request):
-    return render(request, "index.html")
+    products = Product.objects.all()
+    return render(request, "index.html", context={"products": products})
 
 def about(request):
     return render(request, "about.html")
+
+
